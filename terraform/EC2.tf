2da275ed576e7853bca573aaf86aa0ddd01661c4
HifzaZaheer96 <hifza.zaheer@academytrainee.com>
2020-04-29 14:35:52 +0000

    EC2 Terraform

---
+++ terraform/EC2.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "instance_1" {
+  ami                    = var.ami_id
+  instance_type          = var.t2_micro
+  subnet_id              = "${aws_subnet.subnet1.id}"
+  vpc_security_group_ids = ["${aws_security_group.group-project.id}"]
+  key_name               = var.key_name
+}
