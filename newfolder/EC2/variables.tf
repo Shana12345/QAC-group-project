4b9f2dc4cf62de96e39616d1b808ef49ca48f6d2
Ubuntu <ubuntu@ip-172-31-16-235.eu-west-2.compute.internal>
2020-05-01 11:45:15 +0000

    everything is together

---
+++ newfolder/EC2/variables.tf
@@ -0,0 +1,20 @@
+variable "aws_subnetsEC2" {
+  
+}
+
+variable "sg" {
+  
+}
+
+variable "ami_id" {
+  default = "ami-08c757228751c5335"
+
+}
+
+variable "t2_micro" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "hifza-zaheer"
+}
