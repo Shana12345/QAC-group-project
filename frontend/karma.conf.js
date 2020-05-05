56592aec01c981fc5de6dace9fb3bd204bb51edb
Finn969 <faustinmacrae@gmail.com>
2020-05-05 17:42:05 +0100

    edited frontend for automated jenkins testing

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -8,13 +8,18 @@ module.exports = function (config) {
     plugins: [
       require('karma-coverage'),
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-chrome-launcher'),      
+      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: true // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
+      fixWebpackSourcePaths: true
     },
     coverageReporter:{
       type:'text',
@@ -27,6 +32,6 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['PhantomJS'],
-    singleRun: false
+    singleRun: true
   });
 };
