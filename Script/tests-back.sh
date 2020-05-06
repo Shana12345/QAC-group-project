0e34eaeda038da8bd16939b0e994c430b39911f4
Finn969 <faustinmacrae@gmail.com>
2020-05-06 19:34:20 +0100

    removed ref to project-folder

--- Script/tests-back.sh
+++ Script/tests-back.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rm -rf project-folder
-git clone https://github.com/Finn969/QAC-group-project.git project-folder
+#rm -rf project-folder
+#git clone https://github.com/Finn969/QAC-group-project.git project-folder
 git checkout Finn
-cd project-folder
+#cd project-folder
 cd backend
 mvn test
--- Script/tests-front.sh
+++ Script/tests-front.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rm -rf project-folder
-git clone https://github.com/Finn969/QAC-group-project.git project-folder
+#rm -rf project-folder
+#git clone https://github.com/Finn969/QAC-group-project.git project-folder
 git checkout Finn
-cd project-folder
+#cd project-folder
 cd frontend
 ls -al
 npm install
