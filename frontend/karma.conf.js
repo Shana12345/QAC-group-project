586f3d5f637b93f8ab075679382d7552eb4f94c7
Finn969 <faustinmacrae@gmail.com>
2020-05-06 19:13:06 +0100

    altered awscli role

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -6,33 +6,26 @@ module.exports = function (config) {
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require('karma-coverage'),
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    browsers: ['PhantomJS'],
     client:{
-      clearContext: true // leave Jasmine Spec Runner output visible in browser
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
     },
-    coverageReporter:{
-      type:'text',
-      file: 'Coverage.txt'
-    },
-
-    reporters: ['progress', 'kjhtml','coverage'],
+    
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-
-    singleRun: true 
+    browsers: ['Chrome'],
+    singleRun: false
   });
-};
+};s
