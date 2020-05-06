312cf2a448b3690cd96deb8201c7cf70109affb6
Finn969 <faustinmacrae@gmail.com>
2020-05-06 10:53:31 +0100

    temporarily made test files clone my own branch

--- Script/tests-back.sh
+++ Script/tests-back.sh
@@ -2,6 +2,7 @@
 
 rm -rf project-folder
 git clone https://github.com/Finn969/QAC-group-project.git project-folder
+git checkout Finn # TEMPORARY USE ONLY! CHANGE BACK WHEN MERGED!
 cd project-folder
 cd backend
 mvn test
