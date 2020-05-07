64f5c5271be00c957a963ba4af4b19a8a29293b8
Ubuntu <ubuntu@ip-172-31-5-248.eu-west-3.compute.internal>
2020-05-07 09:06:47 +0000

    Improved testing

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -6,7 +6,6 @@ module.exports = function (config) {
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require('karma-coverage'),
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-phantomjs-launcher'),
@@ -14,20 +13,17 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
     client:{
-      clearContext: true // leave Jasmine Spec Runner output visible in browser
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true
     },
-    coverageReporter:{
-      type:'text',
-      file: 'Coverage.txt'
-    },
+    
 
-    reporters: ['progress', 'kjhtml','coverage'],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
